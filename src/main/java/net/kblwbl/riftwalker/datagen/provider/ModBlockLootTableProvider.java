befe337c0f6aaea49e6b2bc0ffd48250b29231b6
kibblewibble <jacksonbkilgore@gmail.com>
2026-05-16 01:57:34 -0400

    Unsent change in ModBlockLootTableProvider.java
    # CHANGES
    ~ Modify original code to (attempt to) only have the top Riftshade crop block drop petals.
    
    # IMPORTANT
    Loot Table *should* work. However, it will drop seeds. Still need to test this as I'm just cutting it to go to bed. I'll figure it out in the morning.

--- src/main/java/net/kblwbl/riftwalker/datagen/provider/ModBlockLootTableProvider.java
+++ src/main/java/net/kblwbl/riftwalker/datagen/provider/ModBlockLootTableProvider.java
@@ -2,7 +2,11 @@ package net.kblwbl.riftwalker.datagen.provider;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.kblwbl.riftwalker.block.RiftshadeCropBlock;
 import net.kblwbl.riftwalker.registry.ModBlocks;
+import net.kblwbl.riftwalker.registry.ModItems;
+import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
+import net.minecraft.predicate.StatePredicate;
 import net.minecraft.registry.RegistryWrapper;
 
 import java.util.concurrent.CompletableFuture;
@@ -14,6 +18,9 @@ public class ModBlockLootTableProvider extends FabricBlockLootTableProvider {
     @Override
     public void generate() {
         // Drops Itself \\
-        addDrop(ModBlocks.RIFT_ROSE);
+        BlockStatePropertyLootCondition.Builder builder = BlockStatePropertyLootCondition.builder(ModBlocks.RIFTSHADE_CROP).properties(StatePredicate.Builder.create()
+                .exactMatch(RiftshadeCropBlock.AGE, 8));
+
+        addDrop(ModBlocks.RIFTSHADE_CROP, cropDrops(ModBlocks.RIFTSHADE_CROP, ModItems.RIFTSHADE_PETAL, ModItems.RIFTSHADE_SEEDS, builder));
     }
 }
